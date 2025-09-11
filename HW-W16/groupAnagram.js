fab861d4dfce21fb83a74e1c019b08c0d4270c79
bubumariia <mariiakudaiberdieva@gmail.com>
2025-09-11 10:46:20 +0300

    added leetcode solution

--- HW-W16/groupAnagram.js
+++ HW-W16/groupAnagram.js
@@ -1,3 +1,17 @@
 var groupAnagrams = function(strs) {
-    
+  let hashMap = new Map();
+
+  for (let i = 0; i < strs.length; i++) {
+    let key = strs[i].split("").sort().join("");
+
+    if (hashMap.has(key)) {
+      hashMap.get(key).push(strs[i]);
+    } else {
+      hashMap.set(key, [strs[i]]);
+    }
+  }
+
+  return Array.from(hashMap.values());
 };
+
+console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]));
