7b49f5d5a4757a13445063c77e71a100572a507d
bubumariia <mariiakudaiberdieva@gmail.com>
2025-09-13 01:30:56 +0300

    added leetcode solution

--- HW-W16/longestConsecutiveSequence.js
+++ HW-W16/longestConsecutiveSequence.js
@@ -1,3 +1,28 @@
 var longestConsecutive = function(nums) {
-    
+    if (nums.length === 0) return 0
+    let hashMap = new Map()
+
+    for(let i = 0; i < nums.length; i++){
+        if(!hashMap.has(nums[i])){
+            hashMap.set(nums[i], nums[i])
+        }
+    }
+    let sortedArray = [...hashMap.values()].sort((a, b) => a - b);
+    let count = 1
+    let countList = []
+
+    for(let j = 1; j < sortedArray.length; j++){
+        if(sortedArray[j] === sortedArray[count-1]+1){
+            count++
+        }else{
+            countList.push(count)
+            count = 1
+        }
+    }
+    countList.push(count)
+
+
+    return Math.max(...countList)
 };
+
+console.log(longestConsecutive([100,2]))
