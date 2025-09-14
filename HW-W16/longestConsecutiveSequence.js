0737db18e0b461838c3470639dc2b7c8e0b653b8
bubumariia <mariiakudaiberdieva@gmail.com>
2025-09-14 17:32:10 +0300

    solved leetcode problem and added second solution

--- HW-W16/longestConsecutiveSequence.js
+++ HW-W16/longestConsecutiveSequence.js
@@ -1,28 +1,51 @@
+// var longestConsecutive = function(nums) {
+//     if (nums.length === 0) return 0
+//     let hashMap = new Map()
+
+//     for(let i = 0; i < nums.length; i++){
+//         if(!hashMap.has(nums[i])){
+//             hashMap.set(nums[i], nums[i])
+//         }
+//     }
+//     let sortedArray = [...hashMap.values()].sort((a, b) => a - b);
+//     let count = 1
+//     let countList = []
+
+//     for(let j = 1; j < sortedArray.length; j++){
+//         if(sortedArray[j] === sortedArray[count-1]+1){
+//             count++
+//         }else{
+//             countList.push(count)
+//             count = 1
+//         }
+//     }
+//     countList.push(count)
+
+
+//     return Math.max(...countList)
+// };
+
+// console.log(longestConsecutive([100,2]))
+
 var longestConsecutive = function(nums) {
-    if (nums.length === 0) return 0
-    let hashMap = new Map()
+  if (nums.length === 0) return 0;
 
-    for(let i = 0; i < nums.length; i++){
-        if(!hashMap.has(nums[i])){
-            hashMap.set(nums[i], nums[i])
-        }
+  let numSet = new Set(nums);
+  let longest = 0;
+
+  for (let num of numSet) {
+    if (!numSet.has(num - 1)) {
+      let currentNum = num;
+      let streak = 1;
+
+      while (numSet.has(currentNum + 1)) {
+        currentNum++;
+        streak++;
+      }
+
+      longest = Math.max(longest, streak);
     }
-    let sortedArray = [...hashMap.values()].sort((a, b) => a - b);
-    let count = 1
-    let countList = []
+  }
 
-    for(let j = 1; j < sortedArray.length; j++){
-        if(sortedArray[j] === sortedArray[count-1]+1){
-            count++
-        }else{
-            countList.push(count)
-            count = 1
-        }
-    }
-    countList.push(count)
-
-
-    return Math.max(...countList)
+  return longest;
 };
-
-console.log(longestConsecutive([100,2]))
