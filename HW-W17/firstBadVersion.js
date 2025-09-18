a3fa5718bbc9042eedbbcffb486b731c295b7618
bubumariia <mariiakudaiberdieva@gmail.com>
2025-09-18 12:06:16 +0300

    added solution

---
+++ HW-W17/firstBadVersion.js
@@ -0,0 +1,15 @@
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let left = 0
+        let right = n -1
+
+        while(left <= right){
+            let middle = (left + right) / 2
+            
+        }
+    };
+};
