25d186f3e992cd2c713e56fa6e28cf7cb91c4c40
bubumariia <mariiakudaiberdieva@gmail.com>
2025-09-19 01:24:27 +0300

    started solving Binary Search problems, understanding core idea of it

---
+++ Solved Preoblems/search.js
@@ -0,0 +1,20 @@
+var search = function(nums, target) {
+    let left = 0
+    let right = nums.length - 1
+
+    while(left <= right){
+        let middle = Math.floor((right + left) / 2)
+
+        if(nums[middle] === target){
+            return middle
+        }else if(nums[middle] < target){
+            left = middle +1 
+        }else if(nums[middle] > target){
+            right = middle - 1
+        }
+
+    }
+    return -1
+};
+
+console.log(search([-1,0,3,5,9,12], 9))
