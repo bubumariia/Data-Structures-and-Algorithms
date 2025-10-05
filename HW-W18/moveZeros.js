6c72eb530f6589f9d728f6efd586e42bf066c944
bubumariia <mariiakudaiberdieva@gmail.com>
2025-10-05 18:41:21 +0300

    added solution

---
+++ HW-W18/moveZeros.js
@@ -0,0 +1,17 @@
+var moveZeroes = function(nums) {
+  if(nums.length == 1) return nums
+  let temp = 0
+  
+  for( let i = 0; i < nums.length; i++){
+   if(nums[i] != 0){
+     nums[temp] = nums[i]
+     temp++
+   }
+ }
+  for(let j = temp; j < nums.length; j++ ){
+    nums[j] = 0
+  }
+   return nums
+};
+
+console.log(moveZeroes([0,1,0,3,12]))
