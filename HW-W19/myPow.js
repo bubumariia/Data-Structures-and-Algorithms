ac453024bff705ac2ac88c048519d9a888edbe38
bubumariia <mariiakudaiberdieva@gmail.com>
2025-10-09 15:17:51 +0300

    added solution

---
+++ HW-W19/myPow.js
@@ -0,0 +1,13 @@
+var myPow = function(x, n) {
+    if(n === 0) return 1
+    if(n < 0) {
+        
+        return 1 / myPow(x, -n)
+    }
+    if(n > 0){
+        return x * myPow(x, n-1)
+    }
+    
+};
+
+console.log(myPow(2,-2))
