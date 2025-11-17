4f748c07d50eea20530514567ca5226ffab540b3
bubumariia <mariiakudaiberdieva@gmail.com>
2025-11-18 02:06:10 +0300

    first approach of solving Fibonacci number

---
+++ HW-W19/fibonacciNumber.js
@@ -0,0 +1,18 @@
+var fib = function(n) {
+    if (n === 0) return 0;
+    if(n == 1) return 1
+    let firstPrev = 0
+    let secondPrev = 1
+    let output = 0 
+    
+
+    for(let i = 1; i < n; i++){
+        let fib = firstPrev + secondPrev
+        firstPrev = secondPrev
+        secondPrev = fib
+        output = fib
+    }
+    return output
+    
+};
+console.log(fib(4))
